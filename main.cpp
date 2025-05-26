7e2f9c26c7743c6e7454871b590c7e65b5870c1d
zziglet <a01023701973@gmail.com>
2025-05-26 18:10:59 +0900

    feat: add block test

--- main.cpp
+++ main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include "brick/Brick.h"
+#include "brick/BrickEnum.h"
+#include "blocks/base/Block.h"
+
+using namespace std;
 
 int main()
 {
-  std::cout << "Tetris project compiled successfully!" << std::endl;
+  cout << "Tetris project compiled successfully!" << endl;
+
+  Block block(BrickEnum::TBrick, 0, 0);
+
+  for (int i = 0; i < 4; i++) {
+	  for (int j = 0; j < 4; j++) {
+		  cout << (block.getBrick(i, j) != BrickEnum::EmptyBrick ? "бс " : "бр ");
+	  }
+
+	  cout << endl;
+  }
+
+  block.spin();
+
+  for (int i = 0; i < 4; i++) {
+	  for (int j = 0; j < 4; j++) {
+		  cout << (block.getBrick(i, j) != BrickEnum::EmptyBrick ? "бс " : "бр ");
+	  }
+
+	  cout << endl;
+  }
+
   return 0;
 }
