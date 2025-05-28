00744932b6d10bb7ffb092a8c0665d7cfbf3dede
shinyedam <shinyedam10008@gmail.com>
2025-05-28 16:28:11 +0900

    Update Renderer.h

--- presentation/Renderer.h
+++ presentation/Renderer.h
@@ -1,1 +1,15 @@
 // Renderer.h
+#pragma once
+#include <string>
+#include <vector>
+
+class Renderer {
+public:
+    void showMainMenu();
+    void showStageSelect(int coin);
+    void showGameScreen(const std::vector<std::string>& boardView, int score, int coin);
+    void showScore(int score, int coin);
+    void showStageResult(bool success, int earnedCoin);
+    void showEnding();
+    void showMessage(const std::string& message);
+};
