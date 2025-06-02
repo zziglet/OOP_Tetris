cd5122b1f6dc57d56ec6e6fee1ae061875cf4ace
shinyedam <shinyedam10008@gmail.com>
2025-06-03 04:11:54 +0900

    Update ScoreManager.h

--- domain/ScoreManager.h
+++ domain/ScoreManager.h
@@ -1,1 +1,16 @@
 // ScoreManager.h
+#pragma once
+
+class ScoreManager {
+private:
+    int score;
+    int currency;
+
+public:
+    ScoreManager();
+    void addScore(int amount);
+    void addCurrency(int amount);
+    void reset();
+    int getScore() const;
+    int getCurrency() const;
+};
