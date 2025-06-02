5f2569c78567c63e6a933145cdc262d565f7f4fb
shinyedam <shinyedam10008@gmail.com>
2025-06-03 04:12:22 +0900

    Update ScoreManager.cpp

--- domain/ScoreManager.cpp
+++ domain/ScoreManager.cpp
@@ -1,1 +1,25 @@
 // ScoreManager.cpp
+#include "ScoreManager.h"
+
+ScoreManager::ScoreManager() : score(0), currency(0) {}
+
+void ScoreManager::addScore(int amount) {
+    score += amount;
+}
+
+void ScoreManager::addCurrency(int amount) {
+    currency += amount;
+}
+
+void ScoreManager::reset() {
+    score = 0;
+    currency = 0;
+}
+
+int ScoreManager::getScore() const {
+    return score;
+}
+
+int ScoreManager::getCurrency() const {
+    return currency;
+}
