1ec432ab560c771bdaf87930f118fbb53e261a92
zziglet <a01023701973@gmail.com>
2025-06-04 02:46:51 +0900

    fix: add constants
    - currency 추가
    - speed 추가

--- utils/Constants.h
+++ utils/Constants.h
@@ -4,6 +4,12 @@ namespace Constants {
     constexpr int STAGE1_DURATION = 120;
     constexpr int STAGE2_DURATION = 90;
     constexpr int STAGE3_DURATION = 60;
+    constexpr int STAGE1_CURRENCY = 0;
+    constexpr int STAGE2_CURRENCY = 30;
+    constexpr int STAGE3_CURRENCY = 50;
+    constexpr int STAGE1_SPEED = 300;
+    constexpr int STAGE2_SPEED = 400;
+    constexpr int STAGE3_SPEED = 500;
     constexpr int BOMB_SCORE_THRESHOLD = 5000;
     constexpr int ENERGY_CORE_TURN = 6;
 }
