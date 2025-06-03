6c39b3b36cec8ac77a93b1957a9507f25700106e
shinyedam <shinyedam10008@gmail.com>
2025-06-03 18:08:29 +0900

    Update Renderer.h

--- presentation/Renderer.h
+++ presentation/Renderer.h
@@ -1,21 +1,25 @@
-//Renderer.h
+// Renderer.h
 #pragma once
-#include "Board.h"
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include "Board.h"
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
 
 class Renderer {
 public:
-    void drawSelectStage(int stage);
-    void drawBoard(const Board& board);
-    void drawScoreBar(int score, int stabilizer, int remainingTime); //stabilizer(안정시켜주는 장치?) = 재화
-    void showStageClear(int stage, int stabilizer);
-    void showGameOver();
-    void showIntro();
-    void showStory();
-    void showInsufficientCurrency(int required);
-    void showStageEntryConfirm(int required);
-    int gotoxy(int x, int y);
-    void SetColor(int color);
+    static void gotoXY(int x, int y);
+    static void drawSelectStage(int stageIndex);
+    static void drawBoard(const Board& board);
+    static void drawScoreBar(int score, int stabilizer, int remainingTime);
+    static void showStageClear(int stage, int stabilizer);
+    static void showGameOver();
+    static void showIntro();
+    static void showStory();
+    static void showInsufficientCurrency(int required);
+    static void showStageEntryConfirm(int required);
 };
