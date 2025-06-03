24b99d6bc05321381c78817d0c4f84f3661d74ce
zziglet <a01023701973@gmail.com>
2025-06-04 02:48:15 +0900

    fix: fix main routine
    - main에 gamemanager 반영

--- main.cpp
+++ main.cpp
@@ -1,82 +1,23 @@
-#include <iostream>
-#include "brick/Brick.h"
-#include "brick/BrickEnum.h"
-#include "block/Block.h"
-#include "Stage.h"
-#include "BlockGenerator.h"
-#include "Board.h"
-#include "Renderer.h"
+#include "GameManager.h"
 
-
-using namespace std;
-
-void printBlock(Block block);
-void printBoard(Board A);
-
-int main()
-{
-	cout << "Tetris project compiled successfully!" << endl;
-
-	Block block(BrickEnum::TBrick, 0, 0);
-
-	printBlock(block);
-	block.spin();
-	printBlock(block);
-
-	cout << "BlockGenerator, Stage Test" << endl;
-
-	Stage stage = Stage(1,1000,1000,5000,6);
-
-	BlockGenerator generator = BlockGenerator(stage);
-
-	for (int i = 0; i < 1000; i++) {
-		BrickEnum A = (*generator.getNextBlock(0, 0)).getBrickType();
-
-		if (A == BrickEnum::BombBrick || A == BrickEnum::EmptyBrick || A == BrickEnum::EnergyBrick) {
-			cout << "Error is occured during generating block" << endl;
-			break;
-		}
-	}
-
-	for (int i = 0; i < 7; i++) 
-		printBlock(*generator.getNextBlock(7500, 9));
-
-	for (int i = 0; i < 7; i++)
-		printBlock(*generator.getNextBlock(6000, 7));
-
-	Board A{};
-	
-	//A.render();
-	int currTurn = 0;
-	Block* temp = generator.getNextBlock(currTurn, 0);
-	A.setNextBlock(temp, 0);
-
-	Renderer R;
-	R.showStory();
-
-  return 0;
+int main() {
+    GameManager gameManager;
+    gameManager.startGame();
+    return 0;
 }
 
+//int main() {
+//    Stage stage(1, 1000, 1000, 5000, 6);
+//    BlockGenerator generator(stage);
+//    Board board;
+//
+//    Block* block = generator.getNextBlock(0, 0);
+//    block->r = 5; block->c = 5; // 중간쯤 위치시키기
+//    board.setNextBlock(block, 0);
+//
+//    Renderer renderer;
+//    renderer.drawBoard(board); // 블럭 보이는지 확인
+//
+//    return 0;
+//}
 
-void printBoard(Board A) {
-	static int cnt = 1;
-
-	cout << "--------" << cnt++ << "--------" << endl;
-	A.render();
-}
-
-void printBlock(Block block) {
-	static int cnt = 1;
-
-	cout << "--------" << cnt++ << "--------" << endl;
-	
-
-	for (int i = 0; i < 4; i++) {
-		for (int j = 0; j < 4; j++) {
-			cout << (block.getBrick(i, j) != BrickEnum::EmptyBrick ? "■ " : "□ ");
-		}
-
-		cout << endl;
-	}
-
-}
